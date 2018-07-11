014ad344665b79cbdb544c5dc462f48f99507124
cylai0723@gmail.com <cylai0723@gmail.com>
2018-07-11 09:05:53 +0800

    add portfolio api

--- router/PortfolioRouter.js
+++ router/PortfolioRouter.js
@@ -8,6 +8,7 @@ module.exports = class PortfolioRouter {
     router(){
         let router = express.Router();
         router.get('/',this.getPortfolios.bind(this));
+        router.get('/:id',this.getTransactions.bind(this));
         return router;
     }
 
@@ -16,4 +17,32 @@ module.exports = class PortfolioRouter {
         .then((results)=>res.json(results))
         .catch((err) => res.status(500).json(err));
     }
+
+    getTransactions(req,res){
+        let objOfResults;
+        return Promise.all([this.portfolioService.listTransinP(req.params.id),
+            this.portfolioService.getStockPositions(req.params.id)])
+        .then((arrayOfResults)=>{
+            
+            objOfResults = {
+                transactions: arrayOfResults[0],
+                positions: arrayOfResults[1]
+            }
+            
+            let stockList = [];
+            for (let i=0;i<objOfResults.positions.length;i++){
+                stockList.push(objOfResults.positions[i].asset_symbol);
+            }
+            return this.portfolioService.updateStockPrice(stockList);
+            
+        }).then((listOfPrice)=>{
+            console.log(listOfPrice);
+            for (let i=0;i<objOfResults.positions.length;i++){
+                objOfResults.positions[i].current_price = listOfPrice[i];
+            }
+            res.json(objOfResults);
+        })
+        .catch((err)=> res.status(500).json(err));
+    }
+
 }
