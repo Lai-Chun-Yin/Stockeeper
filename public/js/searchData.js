f0f77c273f45f470590ea8b4da8cd3e845b95605
siuhangw <wshiahb@gmail.com>
2018-07-16 15:36:56 +0800

    Add dummy data from search function

---
+++ public/js/searchData.js
@@ -0,0 +1,281 @@
+var data = {
+    "item": [
+        {
+            "Symbol": "HKG:0005",
+            "Company": "HSBC Holdings plc",
+            "Current_Price": "$73.90",
+            "Status": "$+0.55(0.75%)"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:0700",
+            "Company": "Tencent",
+            "Current_Price": "$389",
+            "Status": "unknow"
+        },
+        {
+            "Symbol": "HKG:1337",
+            "Company": "Razer",
+            "Current_Price": "$1.8",
+            "Status": "unknow"
+        }
+    ]
+};
+
+// $(document).ready(function(){
+//     var template = $("#template").html();
+//     console.log(template);
+//     var templateScript = Handlebars.compile(template);
+//     console.log(templateScript);
+//     var html = (templateScript(data.item[0]));
+//     console.log(data.item);
+//     console.log(templateScript(data.item));
+//     $(".display").append(html);
+// });
+
+$(document).ready(function(){
+    let templateScript = Handlebars.compile(`
+    {{#each item}}
+        <tr>
+            <td>{{ Company }}</td>
+            <td>{{ Symbol }}</td>
+            <td>{{ Current_Price }}</td>
+            <td>{{ Status }}</td>
+            <td><button><span class="mif-plus"></span></button></td>
+        </tr>
+    {{/each}}
+    `);
+    // console.log(templateScript);
+    var html = (templateScript(data));
+    // console.log(data.item);
+    // console.log(templateScript(data.item));
+    // $("tbody").append(html);
+    $(".table tbody").append(html);
+});
