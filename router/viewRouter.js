4a9a997d5ea764d81b7109f9eaa88eaa568af5e8
siuhangw <wshiahb@gmail.com>
2018-07-22 02:25:25 +0800

    Page redirecting change

--- router/viewRouter.js
+++ router/viewRouter.js
@@ -10,9 +10,9 @@ module.exports = (express) => {
 
     async function isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) {
-          // as soon as user logged in, retrieve portfolio list
-          portfoList = await ps.listPortfolios(req.session.passport.user)
-          return next();
+            // as soon as user logged in, retrieve portfolio list
+            portfoList = await ps.listPortfolios(req.session.passport.user)
+            return next();
         }
 
         // res.redirect('/login');
@@ -27,9 +27,14 @@ module.exports = (express) => {
     });
 
     router.get('/',isLoggedIn, (req, res) => {
-        res.render("index", {
-            pageTitle: 'Index',
-            pageID: 'index',
+        // res.render("index", {
+        //     pageTitle: 'Index',
+        //     pageID: 'index',
+        //     portfoList: portfoList
+        // });
+        res.render('home', {
+            pageTitle: 'Home',
+            pageID: 'home',
             portfoList: portfoList
         });
     });
@@ -38,16 +43,30 @@ module.exports = (express) => {
         res.render('login', {
             pageTitle: 'Login',
             pageID: 'login',
+            "message-display":"none"
         });
     });
 
-    router.post('/login', passport.authenticate('local-login', {
-        successRedirect: '/index',
-        failureRedirect: '/login',
-        failureFlash: true // Disable flash
-    }));
+    router.post('/login', function (req, res, next) {
+        passport.authenticate('local-login', function (err, user, info) {
+            if (err) { return next(err); }
+            if (!user) {
+                // req.session.message = info.message;
+                return res.render('login', {
+                    pageTitle: 'Login',
+                    pageID: 'login',
+                    message: info.message,
+                    "message-display":"block"
+                });
+            }
+            req.logIn(user, function (err) {
+                if (err) { return next(err); }
+                return res.redirect('/home');
+            });
+        })(req, res, next);
+    });
 
-    router.get('/signup', (req,res)=>{
+    router.get('/signup', (req, res) => {
         res.render('signup', {
             pageTitle: 'Signup',
             pageID: 'signup'
@@ -55,47 +74,47 @@ module.exports = (express) => {
     });
 
     router.post('/signup',passport.authenticate('local-signup', {
-        successRedirect: '/index',
+        successRedirect: '/home',
         failureRedirect: '/signup',
         failureFlash: true
     }));
 
-    router.get("/auth/google",passport.authenticate('googleToken',{
-        scope:['profile','email']
+    router.get("/auth/google", passport.authenticate('googleToken', {
+        scope: ['profile', 'email']
     }));
-    router.get("/auth/google/redirect",passport.authenticate('googleToken',{
+    router.get("/auth/google/redirect", passport.authenticate('googleToken', {
         failureRedirect: "/"
-    }),(req,res)=>res.redirect('/index'));
+    }),(req,res)=>res.redirect('/home'));
 
-    router.get("/auth/facebook",passport.authenticate('facebook',{
-        scope: ['user_friends', 'manage_pages'] 
+    router.get("/auth/facebook", passport.authenticate('facebook', {
+        scope: ['user_friends', 'manage_pages']
     }));
-    router.get("/auth/facebook/callback",passport.authenticate('facebook',{
+    router.get("/auth/facebook/callback", passport.authenticate('facebook', {
         failureRedirect: "/"
-    }),(req,res)=>res.redirect('/index'));
+    }),(req,res)=>res.redirect('/home'));
 
-    router.get('/index',isLoggedIn, (req, res) => {
-        res.render("index", { 
-          username: req.user.username,
-          pageTitle: 'Index',
-          pageID: 'index',
-          portfoList: portfoList
-        });
-    });
+    // router.get('/index',isLoggedIn, (req, res) => {
+    //     res.render("index", { 
+    //       username: req.user.username,
+    //       pageTitle: 'Index',
+    //       pageID: 'index',
+    //       portfoList: portfoList
+    //     });
+    // });
 
-    router.get('/logout', function(req, res){
+    router.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
 
-    router.get('/home', function(req, res){
+    router.get('/home', function (req, res) {
         res.render('home', {
             pageTitle: 'Home',
             pageID: 'home',
             portfoList: portfoList
         });
     });
-    router.get('/search',isLoggedIn, function(req, res){
+    router.get('/search', isLoggedIn, function (req, res) {
         res.render('search', {
             pageTitle: 'Search',
             pageID: 'search',
@@ -103,7 +122,7 @@ module.exports = (express) => {
         });
     });
 
-    router.get('/portfolio',isLoggedIn, function(req,res){
+    router.get('/portfolio', isLoggedIn, function (req, res) {
         res.render('portfolio', {
             pageTitle: 'Portfolio',
             pageID: 'portfolio',
@@ -111,20 +130,20 @@ module.exports = (express) => {
         });
     });
 
-    router.get('/chat',isLoggedIn, function(req,res){
-      res.render('chat', {
-          pageTitle: 'Chat Room',
-          pageID: 'chat',
-          portfoList: portfoList
+    router.get('/chat', isLoggedIn, function (req, res) {
+        res.render('chat', {
+            pageTitle: 'Chat Room',
+            pageID: 'chat',
+            portfoList: portfoList
         });
     });
 
-    router.get('/stock/:symbol',isLoggedIn,function(req,res){
-        res.render('stock',{symbol:req.params.symbol})
+    router.get('/stock/:symbol', isLoggedIn, function (req, res) {
+        res.render('stock', { symbol: req.params.symbol })
     });
 
-    router.get('/addTran/:symbol',isLoggedIn, function(req,res){
-        res.render('addTran',{symbol:req.params.symbol})
+    router.get('/addTran/:symbol', isLoggedIn, function (req, res) {
+        res.render('addTran', { symbol: req.params.symbol })
     });
 
     return router;
