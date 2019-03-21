a1fbdfed1fe5042b4f2d34e47a1a9afcc7d7364d
Tommy Lai <cylai0723@gmail.com>
2019-03-22 00:01:55 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,4 @@ git remote add google \
 https://source.developers.google.com/p/Stockeeper/r/Stockeeper
 git push --all google
 # ssh to GCP
-gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" \
---command "docker stack deploy -c docker-compose.test.yml stockeeper"
+gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" -- "docker stack deploy -c docker-compose.test.yml stockeeper"
