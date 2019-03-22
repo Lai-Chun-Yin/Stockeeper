dda548005aac31fa2846dc746baa3dd81a8e50c1
Tommy Lai <cylai0723@gmail.com>
2019-03-22 16:16:29 +0800

    test again

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 docker build -t tommylcy/sk-nginx -f ./nginx/Dockerfile.dev ./nginx
 docker push tommylcy/sk-nginx
 docker push tommylcy/sk-website
-git config credential.helper gcloud.sh
-git remote add google \
-https://source.developers.google.com/p/Stockeeper/r/Stockeeper
-git push --all google
 # ssh to GCP
 nohup gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" -- "docker stack deploy -c docker-compose.test.yml stockeeper" &
