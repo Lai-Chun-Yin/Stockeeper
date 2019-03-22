06348df9bea66d44ae5e6b8d1283b3d94729ba76
Tommy Lai <cylai0723@gmail.com>
2019-03-22 17:09:36 +0800

    re-test

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ docker build -t tommylcy/sk-nginx -f ./nginx/Dockerfile.dev ./nginx
 docker push tommylcy/sk-nginx
 docker push tommylcy/sk-website
 # ssh to GCP
-nohup gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" -- "docker stack deploy -c docker-compose.test.yml stockeeper" &
+nohup gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "docker stack deploy -c docker-compose.test.yml stockeeper" &
