2e1cc12e04032b59654cfbf357f1f4c5c215ffb1
Tommy Lai <cylai0723@gmail.com>
2019-03-24 12:37:41 +0800

    modify git install

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t asia.gcr.io/stockeeper-210714/sk-nginx -f ./Stockeeper/nginx/Dockerfile.dev ./Stockeeper/nginx
+docker build -t asia.gcr.io/stockeeper-210714/sk-nginx -f ./nginx/Dockerfile.dev ./nginx
 docker push asia.gcr.io/stockeeper-210714/sk-nginx
 docker push asia.gcr.io/stockeeper-210714/sk-website
 # ssh to GCP
