d5f754dcc12b738102c99fa508c70bec0fa745fa
Tommy Lai <cylai0723@gmail.com>
2019-03-24 18:24:19 +0800

    add test dockerfile

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 docker build -t asia.gcr.io/stockeeper-210714/sk-nginx -f ./nginx/Dockerfile.dev ./nginx
+docker build -t asia.gcr.io/stockeeper-210714/sk-website -f ./Dockerfile .
 docker push asia.gcr.io/stockeeper-210714/sk-nginx
 docker push asia.gcr.io/stockeeper-210714/sk-website
 # ssh to GCP
