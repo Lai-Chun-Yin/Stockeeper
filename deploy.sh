8bdb1d5ca93fa3d53c882cacece8967f5082b10a
Tommy Lai <cylai0723@gmail.com>
2019-03-25 16:05:46 +0800

    test42

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-docker build -t asia.gcr.io/stockeeper-210714/sk-nginx -f ./nginx/Dockerfile.dev ./nginx
-docker build -t asia.gcr.io/stockeeper-210714/sk-website -f ./Dockerfile .
+docker build -t sk-nginx -f ./nginx/Dockerfile.dev ./nginx
+docker build -t sk-website -f ./Dockerfile .
+docker tag sk-nginx asia.gcr.io/stockeeper-210714/sk-nginx
+docker tag sk-nginx asia.gcr.io/stockeeper-210714/sk-website
 docker push asia.gcr.io/stockeeper-210714/sk-nginx
 docker push asia.gcr.io/stockeeper-210714/sk-website
 # ssh to GCP
