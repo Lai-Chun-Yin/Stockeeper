1cee6201bf34f16dee15cf7f7e951c04923c4eac
Tommy Lai <cylai0723@gmail.com>
2019-03-25 10:43:55 +0800

    test escape

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker build -t asia.gcr.io/stockeeper-210714/sk-website:$SHA -f ./Dockerfile .
 docker push asia.gcr.io/stockeeper-210714/sk-nginx:$SHA
 docker push asia.gcr.io/stockeeper-210714/sk-website:$SHA
 # ssh to GCP
-gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "env SHA=latest docker stack deploy --with-registry-auth -c docker-compose.test.yml stockeeper"
+gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "env SHA=\"latest\" docker stack deploy --with-registry-auth -c docker-compose.test.yml stockeeper"
