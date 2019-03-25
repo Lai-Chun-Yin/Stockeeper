48416bab96d703da01d46a87a42c1506926ca23b
Tommy Lai <cylai0723@gmail.com>
2019-03-25 15:16:26 +0800

    re-test nohup

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker build -t asia.gcr.io/stockeeper-210714/sk-website -f ./Dockerfile .
 docker push asia.gcr.io/stockeeper-210714/sk-nginx
 docker push asia.gcr.io/stockeeper-210714/sk-website
 # ssh to GCP
-gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "sh stack-deploy.sh"
+nohup gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "sh stack-deploy.sh" &
