0f511c885a8ed32decf48da962f49faed5293b68
Tommy Lai <cylai0723@gmail.com>
2019-03-25 11:51:14 +0800

    test resolve image

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-docker build -t asia.gcr.io/stockeeper-210714/sk-nginx:$SHA -f ./nginx/Dockerfile.dev ./nginx
-docker build -t asia.gcr.io/stockeeper-210714/sk-website:$SHA -f ./Dockerfile .
-docker push asia.gcr.io/stockeeper-210714/sk-nginx:$SHA
-docker push asia.gcr.io/stockeeper-210714/sk-website:$SHA
+docker build -t asia.gcr.io/stockeeper-210714/sk-nginx:latest -f ./nginx/Dockerfile.dev ./nginx
+docker build -t asia.gcr.io/stockeeper-210714/sk-website:latest -f ./Dockerfile .
+docker push asia.gcr.io/stockeeper-210714/sk-nginx:latest
+docker push asia.gcr.io/stockeeper-210714/sk-website:latest
+gcloud compute --project "stockeeper-210714" scp --zone "asia-east1-b" ./docker-compose.test.yml "stockeeper-init":~
 # ssh to GCP
-gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "SHA=\"latest\" docker stack deploy --with-registry-auth -c docker-compose.test.yml stockeeper"
+gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "docker stack deploy --resolve-image always --with-registry-auth -c docker-compose.test.yml stockeeper"
