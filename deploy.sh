b6a095553aa0c8c92306625d33e3560e36ac7e46
Tommy Lai <cylai0723@gmail.com>
2019-03-25 12:32:28 +0800

    test40

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t asia.gcr.io/stockeeper-210714/sk-nginx:$SHA -f ./nginx/Dockerfile.dev ./nginx
-docker build -t asia.gcr.io/stockeeper-210714/sk-website:$SHA -f ./Dockerfile .
-docker push asia.gcr.io/stockeeper-210714/sk-nginx:$SHA
-docker push asia.gcr.io/stockeeper-210714/sk-website:$SHA
+docker build -t asia.gcr.io/stockeeper-210714/sk-nginx -f ./nginx/Dockerfile.dev ./nginx
+docker build -t asia.gcr.io/stockeeper-210714/sk-website -f ./Dockerfile .
+docker push asia.gcr.io/stockeeper-210714/sk-nginx
+docker push asia.gcr.io/stockeeper-210714/sk-website
 # ssh to GCP
-gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "SHA=\"$SHA\" docker stack deploy --with-registry-auth -c docker-compose.test.yml stockeeper"
+gcloud compute --project "stockeeper-210714" ssh --zone "asia-east1-b" "stockeeper-init" --command "sh stack-deploy.sh"
